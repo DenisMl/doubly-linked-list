a2b74afb32dca2d9e39cd3b50c41277d05e960d4
Denis Molodianovich <denisdm95poker@gmail.com>
2017-01-29 23:43:40 +0200

    doubly-linked-list/fix: var names

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,13 +3,20 @@ const Node = require('./node');
 class LinkedList {
     constructor() {
       this.length = 0;
+      this._head = null;
+      this._tail = null;
     }
 
     append(data) {
       const node = new Node(data);
       if(this.length) {
+        this._tail.next = node;
+        node.prev = this._tail;
+        this._tail = node;
       }
       else {
+        this._head = node;
+        this._tail = node;
       }
       this.length++;
       return this;
